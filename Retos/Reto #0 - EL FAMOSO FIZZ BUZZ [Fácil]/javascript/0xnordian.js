2cff38c17ad3d14db69acb72f1c945e881a5c564
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-06-24 16:41:28 +0200

    Update 0xnordian.js

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/javascript/0xnordian.js
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/javascript/0xnordian.js
@@ -1,1 +1,20 @@
+function fizzBuzz(target) {
+  let i = 1;
+  const fizz = "fizz";
+  const buzz = "buzz";
 
+  while (i <= 100) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log(fizz + buzz);
+    } else if (i % 3 === 0) {
+      console.log(fizz);
+    } else if (i % 5 === 0) {
+      console.log(buzz);
+    } else {
+      console.log(i);
+    }
+    i++;
+  }
+}
+
+fizzBuzz(100);
