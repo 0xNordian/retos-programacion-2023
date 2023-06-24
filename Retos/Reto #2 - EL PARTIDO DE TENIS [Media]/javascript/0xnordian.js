8da44ebab60cffd92dd79223dc8da00b89962142
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-06-24 23:23:26 +0200

    Create 0xnordian.js
    Missing input check. WIP

---
+++ Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/javascript/0xnordian.js
@@ -0,0 +1,31 @@
+const points = new Map([
+  [0, "Love"],
+  [1, "15"],
+  [2, "30"],
+  [3, "40"],
+]);
+
+function tenisGame(arr) {
+  let p1 = 0;
+  let p2 = 0;
+  for(let i = 0; i < arr.length; i++) {
+    arr[i] === "P1" ? p1++ : p2++;
+    if(p1 >= 3 && p2 >= 3){
+      if(p1 === p2) {
+        console.log("Deuce")
+      } else if ((p1 - p2 === 1) || (p2 - p1 === 1)){
+        p1 > p2 ? console.log("Ventaja P1") : console.log("Ventaja P2")
+      } else if((p1 - p2 >= 2) || (p2 - p1 >= 2)){
+        if(p1 > p2){
+          console.log("Ha ganado el P1");
+          break;
+        } else {
+          console.log("Ha ganado el P2");
+          break;
+        }
+      } 
+    } else {
+        console.log(`${points.get(p1)} - ${points.get(p2)}`);
+    }
+  }
+}
